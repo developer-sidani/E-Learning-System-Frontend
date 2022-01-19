31ff048844b292494447d96e822527a4bf28b22a
developer-sidani <asidani88@gmail.com>
2022-01-19 14:35:44 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import  Head from "next/Head";
+
+const Home = () => {
+    return (
+        <div>
+        <Head>
+            <title>
+                Hello World!w
+            </title>
+        </Head>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        </div>
+    );
+};
+
+export default Home;
