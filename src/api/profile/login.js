4df38894889410e4e176aaa3d703811df93508f9
developer-sidani <asidani88@gmail.com>
2022-02-24 01:05:23 +0200

    draft - added login api under the profile section that is found in the api directory

---
+++ src/api/profile/login.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { baseUrl } from '../config'
+
+export const login = async ({ email, password }) => {
+  try {
+    const result = await axios.post(`${baseUrl}/userAuth/login`, {
+      email,
+      password,
+    })
+    return {
+      token: result?.data?.token,
+      user: result?.data?.user,
+      status: result.status,
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
