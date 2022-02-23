6f3cab7fe82cc0e37da388cd3cfb3e6702651cda
developer-sidani <asidani88@gmail.com>
2022-02-24 01:08:04 +0200

    update - update in line 6 in login.js file - "fixed the api route"

--- src/api/profile/login.js
+++ src/api/profile/login.js
@@ -3,7 +3,7 @@ import { baseUrl } from '../config'
 
 export const login = async ({ email, password }) => {
   try {
-    const result = await axios.post(`${baseUrl}/userAuth/login`, {
+    const result = await axios.post(`${baseUrl}/auth/login`, {
       email,
       password,
     })
