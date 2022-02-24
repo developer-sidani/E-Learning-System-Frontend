9880736a423403d13afd181fc43556ff113cb66a
developer-sidani <asidani88@gmail.com>
2022-02-25 01:16:03 +0200

    testing authentication

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import '../styles/globals.css'
 import React from 'react'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
 
 export default MyApp
