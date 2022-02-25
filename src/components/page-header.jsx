2737382fd6a7b50b7539fb5c1b073fe73eab381b
developer-sidani <asidani88@gmail.com>
2022-02-25 03:37:13 +0200

    refactor - created page header component, "passing title as props, same goes to the children in case we need to add an icon or a meta tag"

---
+++ src/components/page-header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+
+const PageHeader = ({ title, children }) => (
+    <Head>
+        <title>{title}</title>
+        {children}
+    </Head>
+)
+
+export { PageHeader }
