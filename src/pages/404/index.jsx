cccf8b20d554e316da241e65c7fb8908ad4ebc4f
developer-sidani <asidani88@gmail.com>
2022-02-28 02:39:43 +0200

    Finalizing 404 Page
    Changes:
    - changed the template from tailwind
    - centered the div due to the errors from the html tag

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -2,42 +2,38 @@ import React from 'react'
 import { PageHeader } from '../../components'
 
 const Custom404 = () => (
-    <>
-    <PageHeader title="Learn + | 404" />
-    {/*
-      This example requires updating your template:
-
-      ```
-      <html class="h-full">
-      <body class="h-full">
-      ```
-    */}
-    <main
-      className="min-h-full bg-cover bg-top sm:bg-top"
-      style={{
-        backgroundImage:
-          'url("https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75")',
-      }}
-    >
-      <div className="max-w-7xl mx-auto px-4 py-16 text-center sm:px-6 sm:py-24 lg:px-8 lg:py-48">
-        <p className="text-sm font-semibold text-black text-opacity-50 uppercase tracking-wide">404 error</p>
-        <h1 className="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">
-          Uh oh! I think you’re lost.
-        </h1>
-        <p className="mt-2 text-lg font-medium text-black text-opacity-50">
-          It looks like the page you&apos;re looking for doesn&apos;t exist.
-        </p>
-        <div className="mt-6">
-          <a
-            href="#"
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50"
-          >
-            Go back home
-          </a>
+  <>
+  <PageHeader title="Learn+ | 404" />
+  <div className="flex flex-col items-center justify-center">
+  <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    <div className="max-w-max mx-auto">
+      <main className="sm:flex">
+        <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">404</p>
+        <div className="sm:ml-6">
+          <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">Page not found</h1>
+            <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+          </div>
+          <div className="flex-col items-center justify-center mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            <a
+              href="/"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Go back home
+            </a>
+            {/* <a
+              href="#"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Contact support
+            </a> */}
+          </div>
         </div>
-      </div>
-    </main>
-    </>
+      </main>
+    </div>
+  </div>
+  </div>
+  </>
 )
 
 export default Custom404
