db07fed0fc3b7b2b6dd9429112ced787a3312964
aliyassine26 <47665097+aliyassine26@users.noreply.github.com>
2022-03-02 18:06:39 +0200

    SIGN UP
    Changes:
    - Added SignUpSchema
    -Modified Personal Information
    -Modified Notifications

--- src/components/auth/sign-up.jsx
+++ src/components/auth/sign-up.jsx
@@ -5,12 +5,42 @@ import { useRouter } from 'next/router'
 import PhoneInput from 'react-phone-input-2'
 import { isEmail, countryList } from '../../utils'
 import { styles } from './tw-styles'
+import 'react-phone-input-2/lib/material.css'
 
-import 'react-phone-input-2/lib/bootstrap.css'
+const SignUpSchema = Yup.object().shape({
+  user: Yup.string().required('Required'),
+  email: Yup.string().email('Must be a valid email').required('Required'),
+  password: Yup.string().required('Required'),
+  confirm_password: Yup.string().required('Required'),
+  firstname: Yup.string().required('Required'),
+  lastname: Yup.string().required('Required'),
+  birthday: Yup.string().required('Required'),
+  address: Yup.string().required('Required'),
+  country: Yup.string().required('Required'),
+  gender: (gender) => (Boolean) || 'male',
+  isNotified: Yup.boolean().default(true),
+  keepMeUpdated: Yup.boolean().default(true),
+})
+
+const Logo = 'https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
 
 const SignUpComponent = () => (
-
-  <div className="space-y-6 md:m-32">
+  <div className="space-y-6 mt-6 md:m-32">
+<div className={styles.header}>
+          <img
+            className={styles.logo}
+            src={Logo}
+            alt="Learn+"
+          />
+          <h2 className={styles.title}>Sign up to start with Learn +</h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Already have an account?
+              {' '}
+            <a href="./sign-in" className={styles.link}>
+              Login Here
+            </a>
+          </p>
+</div>
       <div className="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6">
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
@@ -120,20 +150,18 @@ const SignUpComponent = () => (
 
                 <div className="col-span-6 sm:col-span-3">
                 <label htmlFor="country" className="block text-sm font-medium text-gray-700">
-                    Country
+                    Gender
                 </label>
                   <select
-                    id="country"
-                    name="country"
-                    autoComplete="country-name"
+                    id="gender"
+                    name="gender"
+                    autoComplete="Gender"
                     className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   >
-                   {countryList.map((x, i) => (
-                     <option key={i}>{x.Name}</option>
-                   ))}
+                  <option>Male</option>
+                  <option>Female</option>
                   </select>
                 </div>
-
                 {/* ************************************************** */}
 
                             {/* PASSWORD */}
@@ -163,48 +191,77 @@ const SignUpComponent = () => (
                     className={styles.inputContainer}
                   />
                 </div>
+
               {/* ************************************************** */}
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                    Country
+                </label>
+                  <select
+                    id="country"
+                    name="country"
+                    autoComplete="country-name"
+                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  >
+                   {countryList.map((x, i) => (
+                     <option key={i}>{x.Name}</option>
+                   ))}
+                  </select>
+              </div>
 
-                            {/* PHONE NUMBER */}
-
-<PhoneInput
-  enableAreaCodes
-/>
-
-{/* ********************** */}
-
-                <div className="col-span-6 sm:col-span-3" />
-
-                <div className="col-span-6">
-                  <label htmlFor="address" className="block text-sm font-medium text-gray-700">
-                    Address
+              <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                          Address
                   </label>
                   <input
                     type="text"
-                    name="address"
-                    id="address"
-                    autoComplete="address"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
                     className={styles.inputContainer}
-                    // className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                   />
-                </div>
+              </div>
+                            {/* PHONE NUMBER */}
+
+{/* ********************** */}
 
               </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                <div className="col-span- mt-8">
+            <PhoneInput
+              inputProps={{
+                name: 'phone',
+                required: true,
+                // enableSearch: true,
+
+              }}
+              excludeCountries="il"
+              country="lb"
+              inputClass={styles.inputContainer2}
+              containerStyle={{ width: '100%' }}
+              enableAreaCodes
+            />
+                </div>
+
+                </div>
+
             </form>
           </div>
         </div>
       </div>
-
+{/* NOTIFICATIONS */}
       <div className="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6">
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
             <h3 className="text-lg font-medium leading-6 text-gray-900">Notifications</h3>
-            <p className="mt-1 text-sm text-gray-500">Decide which communications you'd like to receive and how.</p>
+            <p className="mt-1 text-sm text-gray-500">Decide which communications you would like to receive.</p>
           </div>
           <div className="mt-5 md:mt-0 md:col-span-2">
+
             <form className="space-y-6" action="#" method="POST">
               <fieldset>
-                <legend className="text-base font-medium text-gray-900">By Email</legend>
+                {/* <legend className="text-base font-medium text-gray-900">By Email</legend> */}
                 <div className="mt-4 space-y-4">
                   <div className="flex items-start">
                     <div className="h-5 flex items-center">
@@ -217,9 +274,9 @@ const SignUpComponent = () => (
                     </div>
                     <div className="ml-3 text-sm">
                       <label htmlFor="comments" className="font-medium text-gray-700">
-                        Comments
+                        Newsletter
                       </label>
-                      <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
+                      <p className="text-gray-500">Subscribe to receive recent news, courses, and offers.</p>
                     </div>
                   </div>
                   <div className="flex items-start">
@@ -231,93 +288,31 @@ const SignUpComponent = () => (
                         className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
                       />
                     </div>
+
                     <div className="ml-3 text-sm">
                       <label htmlFor="candidates" className="font-medium text-gray-700">
-                        Candidates
+                        Notifications
                       </label>
-                      <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
+                      <p className="text-gray-500">Receive all Learn+ relevent notifications.</p>
                     </div>
-                  </div>
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="offers"
-                        name="offers"
-                        type="checkbox"
-                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label htmlFor="offers" className="font-medium text-gray-700">
-                        Offers
-                      </label>
-                      <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                    </div>
-                  </div>
-                </div>
-              </fieldset>
-              <fieldset>
-                <div>
-                  <legend className="text-base font-medium text-gray-900">Push Notifications</legend>
-                  <p className="text-sm text-gray-500">These are delivered via SMS to your mobile phone.</p>
-                </div>
-                <div className="mt-4 space-y-4">
-                  <div className="flex items-center">
-                    <input
-                      id="push-everything"
-                      name="push-notifications"
-                      type="radio"
-                      className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                    />
-                    <label htmlFor="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
-                      Everything
-                    </label>
-                  </div>
-                  <div className="flex items-center">
-                    <input
-                      id="push-email"
-                      name="push-notifications"
-                      type="radio"
-                      className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                    />
-                    <label htmlFor="push-email" className="ml-3 block text-sm font-medium text-gray-700">
-                      Same as email
-                    </label>
-                  </div>
-                  <div className="flex items-center">
-                    <input
-                      id="push-nothing"
-                      name="push-notifications"
-                      type="radio"
-                      className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                    />
-                    <label htmlFor="push-nothing" className="ml-3 block text-sm font-medium text-gray-700">
-                      No push notifications
-                    </label>
                   </div>
                 </div>
               </fieldset>
             </form>
+
           </div>
         </div>
       </div>
 
       <div className="flex justify-center content-center">
         <button
-          type="button"
-          className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Cancel
-        </button>
-        <button
           type="submit"
-          className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="ml-3 w-60 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Sign Up
         </button>
       </div>
   </div>
-
 )
 
 export { SignUpComponent }
