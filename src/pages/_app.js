c074ff672e7f62c1076c6d65e441db2cd50335b4
developer-sidani <asidani88@gmail.com>
2022-03-04 17:35:14 +0200

    Redux Persist Configuration to the app
    Changes:
    - Wrapped the component with provider from redux
      and persist gate from redux-oersist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,18 @@
 import '../styles/globals.css'
 import React from 'react'
+// eslint-disable-next-line import/no-unresolved
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import loadStore from '../store'
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
+const { store, persistor } = loadStore()
+
+const MyApp = ({ Component, pageProps }) => (
+    <Provider store={store}>
+        <PersistGate loading={<p>loading</p>} persistor={persistor}>
+            <Component {...pageProps} />
+        </PersistGate>
+    </Provider>
+)
 
 export default MyApp
