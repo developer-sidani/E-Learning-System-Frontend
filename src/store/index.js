2c3028914fcd548b07baf917b6974c7e121dd287
developer-sidani <asidani88@gmail.com>
2022-03-04 16:39:54 +0200

    Redux Configuration
    Changws:
    - installed redux-persist using yarn
    - configured the default storage for our app
    - added the ignored actions imported from the redux-persist library
      to the store middleware
    - added the profile reducer representing the student that is logged in

---
+++ src/store/index.js
@@ -0,0 +1,42 @@
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
+
+import {
+  profileReducer,
+} from '../slices'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: hardSet,
+  version: 1,
+  debug: true,
+}
+
+const persistedProfileReducer = persistReducer(persistConfig, profileReducer)
+
+export default () => {
+  const store = configureStore({
+    reducer: {
+      profile: persistedProfileReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  })
+  const persistor = persistStore(store)
+  return { store, persistor }
+}
