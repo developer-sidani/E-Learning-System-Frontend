568bb995187905aa81172ed91a22d6219a31fc6e
aliyassine26 <47665097+aliyassine26@users.noreply.github.com>
2022-03-10 23:47:32 +0200

    Sign Up
    Changes:
    -Removed image

--- src/components/auth/sign-up.jsx
+++ src/components/auth/sign-up.jsx
@@ -127,7 +127,6 @@ const SignUpComponent = () => {
                         name="user"
                         id="user"
                         className={styles.inputContainer}
-                        // className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
                         placeholder=""
                       />
                        {(errors.user) && touched.user ? (
@@ -136,25 +135,6 @@ const SignUpComponent = () => {
                             </div>
                        ) : null}
                     </div>
-                  </div>
-                </div>
-
-                <div>
-                  <label className="block text-sm font-medium text-gray-700">Photo</label>
-                  <div className="mt-1 flex items-center space-x-5">
-                    <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                      <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                      </svg>
-                    </span>
-                    <input
-                      value={values.photo}
-                      onChange={handleChange}
-                      id="photo"
-                      name="photo"
-                      type="file"
-                      className="bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    />
                   </div>
                 </div>
 
@@ -236,7 +216,7 @@ const SignUpComponent = () => {
                      ) : null}
                   </div>
 
-                  <div className="col-span-6 sm:col-span-3">
+                  <div className="col-span-6 -mt-1 sm:col-span-3">
                   <label htmlFor="gender" className="block text-sm font-medium text-gray-700">
                       Gender
                   </label>
@@ -302,7 +282,7 @@ const SignUpComponent = () => {
                   </div>
 
                 {/* ************************************************** */}
-                <div className="col-span-6 sm:col-span-3">
+                <div className="col-span-6 sm:col-span-3 -mt-1">
                   <label htmlFor="country" className="block text-sm font-medium text-gray-700">
                       Country
                   </label>
