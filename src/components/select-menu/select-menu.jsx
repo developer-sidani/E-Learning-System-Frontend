d339e42b97a035c2d50db5ec3a461a459c22c124
developer-sidani <developer.sidani@gmail.com>
2022-03-12 17:55:48 +0200

    fixed height of the select menu

--- src/components/select-menu/select-menu.jsx
+++ src/components/select-menu/select-menu.jsx
@@ -19,7 +19,7 @@ const SelectMenu = ({
           <Listbox.Label className="block text-sm font-medium text-gray-700">{title}</Listbox.Label>
           <div className="mt-1 relative">
             <Listbox.Button className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-              <span className="block truncate">{value || options[0]}</span>
+              <span className="block truncate h-5">{value}</span>
               <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                 <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
               </span>
