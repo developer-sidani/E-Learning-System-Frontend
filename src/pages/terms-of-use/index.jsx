7514e0c739339106997a6dde1cc67449a86aa481
developer-sidani <developer.sidani@gmail.com>
2022-03-12 15:40:04 +0200

    terms

---
+++ src/pages/terms-of-use/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { PageHeader, TermsOfUseComponent } from '../../components'
+
+const TermsOfUse = () => (
+    <>
+      <PageHeader title="Learn+ | Terms Of Use" />
+      <TermsOfUseComponent />
+    </>
+)
+
+export default TermsOfUse
