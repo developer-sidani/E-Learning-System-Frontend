151fae416288f18f496242026d45243a29b302a0
developer-sidani <developer.sidani@gmail.com>
2022-03-22 05:23:26 +0200

    data stucture of a course

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 import { MainCourseComponent, PageHeader } from '../components'
 import { MainLayout } from '../layouts'
 
+const course = {
+  title: 'Intro to web',
+  img: '',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, obcaecati, officiis. Aperiam asperiores corporis dicta dolor dolorem excepturi inventore odio perspiciatis rerum temporibus, totam velit. Blanditiis culpa quibusdam saepe! Animi!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, obcaecati, officiis. Aperiam asperiores corporis dicta dolor dolorem excepturi inventore odio perspiciatis rerum temporibus, totam velit. Blanditiis culpa quibusdam saepe! Animi!',
+  instructor: 'test test',
+  price: 49.99,
+  rating: 4.3,
+  postDate: new Date(),
+  bestSeller: false,
+  category: 'Web Development',
+}
 const Home = () => (
     <>
       <PageHeader title="Learn+" />
       <h1>Courses:</h1>
-      <MainCourseComponent />
+      <MainCourseComponent course={course} />
     </>
 )
 Home.getLayout = (page) => (
