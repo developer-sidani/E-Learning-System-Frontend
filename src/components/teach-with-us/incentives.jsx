320d6828909b0e13390c1f5c4c9ee664316be92e
Hasan Itani <74467477+HasanItani@users.noreply.github.com>
2022-03-25 15:36:57 +0200

    Update incentives.jsx

--- src/components/teach-with-us/incentives.jsx
+++ src/components/teach-with-us/incentives.jsx
@@ -4,11 +4,11 @@ const incentives = ({ incentivesFunction }) => (
 
  <div className="bg-white">
 
-    <div className="max-w-7xl mx-auto px-4 py-24 sm:px-6 lg:px-8">
+    <div className="max-w-7xl mx-auto px-4 pt-24 sm:px-6 lg:px-8">
       <div className="bg-gray-50 rounded-2xl px-6 py-16 sm:p-16">
         <div className="max-w-xl mx-auto lg:max-w-none">
           <div className="text-center">
-            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+            <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
               So many reasons to start
             </h2>
           </div>
@@ -20,12 +20,12 @@ const incentives = ({ incentivesFunction }) => (
 
                 <div className="sm:flex-shrink-0">
                   <div className="flow-root">
-                    <img className="w-16 h-16 mx-auto" src={incentive.imageSrc} alt="" />
+                    <img className="w-18 h-18 mx-auto" src={incentive.imageSrc} alt="" />
                   </div>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-6 lg:mt-6 lg:ml-0">
-                  <h3 className="text-sm font-medium text-gray-900">{incentive.name}</h3>
-                  <p className="mt-2 text-sm text-gray-500">{incentive.description}</p>
+                  <h3 className="text-2xl font-medium text-gray-900">{incentive.name}</h3>
+                  <p className="mt-2 text-base text-gray-500">{incentive.description}</p>
                 </div>
             </div>
 
