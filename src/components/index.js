fa3aa50ab41f8b38b74bfedda6b592ace503058c
developer-sidani <developer.sidani@gmail.com>
2022-03-27 01:05:52 +0300

    Merge Conflict Resolve

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
+import OnHoverComponent from './on-hover-component'
+
 export { SignInComponent } from './auth'
 export { SignUpComponent } from './auth'
 export { PageHeader } from './page-header'
 export { ContactUsComponent } from './contact-us'
-export { TeachWithUsComponent } from './teach-with-us'
 export { SelectMenu } from './select-menu'
 export { DatePickerComponent } from './date-picker'
 export { FaqComponent } from './faq/faq-component'
 export { Footer } from './footer'
 export { Navbar } from './navbar'
 export { ForgotPasswordComponent } from './auth/forgot-password'
-export { TermsOfUseComponent } from './terms-of-use'
+export { MainCourseComponent, CourseLoading } from './courses'
+export { OnHoverComponent }
