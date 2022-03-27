178dd1cad3b7778b05fdbf8c62080542b5fcc7a5
developer-sidani <developer.sidani@gmail.com>
2022-03-27 18:32:51 +0300

    Select tab component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   PageHeader, MainBanner, CoursesContainer, TopCategoriesComponent, CallToActionComponent,
 } from '../components'
@@ -220,35 +220,88 @@ const topCategories = [
     route: '/categories/photography-and-video',
   },
 ]
-const Home = () => (
-  <>
-    <PageHeader
-      title="Learn+"
-    />
-    <MainBanner />
-    <div className="ml-2 my-3 px-5">
-      <p className="text-3xl font-serif">
-        A broad selection of courses
-      </p>
-      <p className="mt-2 text-l font-serif">
-        Choose from 183,000 online video courses with new additions published every month
-      </p>
-    </div>
-    <CoursesContainer
-      courses={courses}
-      loading={false}
-      title="Grow your software development skills with JavaScript"
-      description="JavaScript is a text-based computer programming language used to make dynamic web pages. A must-learn for aspiring web developers or programmers, JavaScript can be used for features like image carousels, displaying countdowns and timers, and playing media on a webpage. "
-    />
-    <div className="ml-2 my-3 px-5">
-      <p className="text-3xl font-serif">
-        Top Categories
-      </p>
-      <TopCategoriesComponent categories={topCategories} />
-    </div>
-    <CallToActionComponent />
-  </>
-)
+const tabs = [
+  { name: 'My Account', href: '#', current: false },
+  { name: 'Company', href: '#', current: false },
+  { name: 'Team Members', href: '#', current: true },
+  { name: 'Billing', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+const Home = () => {
+  const [selectedTab, setSelectedTab] = useState(tabs[0])
+  return (
+    <>
+      <PageHeader
+        title="Learn+"
+      />
+      <MainBanner />
+      <div className="ml-2 my-3 px-5">
+        <p className="text-3xl font-serif">
+          A broad selection of courses
+        </p>
+        <p className="mt-2 text-l font-serif">
+          Choose from 183,000 online video courses with new additions published every month
+        </p>
+      </div>
+      <div>
+        <div className="sm:hidden">
+          <label htmlFor="tabs" className="sr-only">
+            Select a tab
+          </label>
+          {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+          <select
+            id="tabs"
+            name="tabs"
+            className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            defaultValue={tabs[0].name}
+          >
+            {tabs.map((tab) => (
+              <option key={tab.name}>{tab.name}</option>
+            ))}
+          </select>
+        </div>
+        <div className="hidden sm:block">
+          <div className="border-b border-gray-200">
+            <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+              {tabs.map((tab) => (
+                <button
+                  key={tab.name}
+                  onClick={() => setSelectedTab(tab)}
+                  type="button"
+                  className={classNames(
+                    tab === selectedTab
+                      ? 'border-indigo-500 text-indigo-600'
+                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                    'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm',
+                  )}
+                  aria-current={tab.current ? 'page' : undefined}
+                >
+                  {tab.name}
+                </button>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div>
+      <CoursesContainer
+        courses={courses}
+        loading={false}
+        title="Grow your software development skills with JavaScript"
+        description="JavaScript is a text-based computer programming language used to make dynamic web pages. A must-learn for aspiring web developers or programmers, JavaScript can be used for features like image carousels, displaying countdowns and timers, and playing media on a webpage. "
+      />
+      <div className="ml-2 my-3 px-5">
+        <p className="text-3xl font-serif">
+          Top Categories
+        </p>
+        <TopCategoriesComponent categories={topCategories} />
+      </div>
+      <CallToActionComponent />
+    </>
+  )
+}
 Home.getLayout = (page) => (
   <MainLayout>
         {page}
