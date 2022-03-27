59e2c5796543fe3bcd3385bcec82b9d6ab1c8874
developer-sidani <developer.sidani@gmail.com>
2022-03-27 13:59:29 +0300

    Animate on scroll init, and using it in the main banner

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,24 @@
 import '../styles/globals.css'
-import React from 'react'
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 // eslint-disable-next-line import/no-unresolved
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import loadStore from '../store'
 import { AuthRouter } from '../routes'
-import { SplashScreen } from '../screens'
+import { SplashScreen } from '../screens' // You can also use <link> for styles
 
 const { store, persistor } = loadStore()
 
 const MyApp = ({ Component, pageProps }) => {
+  useEffect(() => {
+    AOS.init({
+      easing: 'ease-out-cubic',
+      once: true,
+      offset: 50,
+    })
+  }, [])
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return (
