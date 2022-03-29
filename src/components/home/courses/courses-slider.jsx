5135f1d6da6dedd8a01d3db91b284ddc6460fdc5
developer-sidani <developer.sidani@gmail.com>
2022-03-30 01:48:38 +0300

    Ui updates

--- src/components/home/courses/courses-slider.jsx
+++ src/components/home/courses/courses-slider.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import {
   CarouselProvider, Slider, Slide, ButtonBack, ButtonNext,
 } from 'pure-react-carousel'
 import { useTheme } from '@mui/material/styles'
-import { useMediaQuery } from '@mui/material'
-import { ArrowLeftIcon } from '@heroicons/react/solid'
+import { createTheme, useMediaQuery } from '@mui/material'
 import { CourseLoading, MainCourseComponent } from '../../courses'
 
 const CoursesSlider = ({ loading, courses }) => {
-  const theme = useTheme()
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        tab: 800,
+        md: 900,
+        lg: 1200,
+      },
+    },
+  })
   const mobileDevice = useMediaQuery(theme.breakpoints.up('sm'))
+  const tabDevice = useMediaQuery(theme.breakpoints.up('tab'))
+  const largeDevice = useMediaQuery(theme.breakpoints.up('lg'))
+  const getSettingsForCarousel = useMemo(() => {
+    if (mobileDevice) {
+      return { visibleSlides: 3, step: 3 }
+    } if (tabDevice) {
+      return { visibleSlides: 2, step: 1 }
+    } if (largeDevice) {
+      return { visibleSlides: 4, step: 4 }
+    }
+    return { visibleSlides: 1, step: 1 }
+  }, [largeDevice, mobileDevice, tabDevice])
   return (
-    <CarouselProvider infinite id="carouselExampleSlidesOnly" className="carousel slide relative" data-bs-ride="carousel" naturalSlideWidth={300} isIntrinsicHeight totalSlides={loading ? 10 : courses.length} visibleSlides={mobileDevice ? 3 : 1} step={mobileDevice ? 3 : 1}>
+    <CarouselProvider infinite id="carouselExampleSlidesOnly" className="carousel slide relative" data-bs-ride="carousel" naturalSlideWidth={300} isIntrinsicHeight totalSlides={loading ? 10 : courses.length} visibleSlides={getSettingsForCarousel.visibleSlides} step={getSettingsForCarousel.step}>
       <ButtonBack
         className="mr-2 carousel-control-next absolute top-1/2 bottom-1/2 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-2 z-50"
         type="button"
       >
-        <svg className="text-white w-10 h-10 bg-primary rounded-full p-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <ArrowLeftIcon />
+        <svg className="text-white w-10 h-10 bg-primary rounded-full p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
         </svg>
       </ButtonBack>
       <Slider className="p-3 my-2">
@@ -40,12 +61,8 @@ const CoursesSlider = ({ loading, courses }) => {
         className="mr-2 carousel-control-next absolute top-1/2 bottom-1/2 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-2 z-50"
         type="button"
       >
-        <svg className="text-white w-10 h-10 bg-primary rounded-full p-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
+        <svg xmlns="http://www.w3.org/2000/svg" className="text-white w-10 h-10 bg-primary rounded-full p-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
         </svg>
       </ButtonNext>
     </CarouselProvider>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -255,7 +255,7 @@ const Home = () => {
           <select
             id="tabs"
             name="tabs"
-            className="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+            className="border-2 border-black block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
             defaultValue={tabs[0].name}
           >
             {tabs.map((tab) => (
