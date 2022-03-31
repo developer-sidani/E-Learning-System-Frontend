b8e9860957ccd00b266e492e3d51723bfc4b2297
developer-sidani <developer.sidani@gmail.com>
2022-04-01 01:32:21 +0300

    Update in the selected tab

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -256,7 +256,8 @@ const Home = () => {
             id="tabs"
             name="tabs"
             className="border-2 border-black block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-            defaultValue={tabs[0].name}
+            value={selectedTab}
+            onChange={(e) => setSelectedTab(e.target.value)}
           >
             {tabs.map((tab) => (
               <option key={tab.name}>{tab.name}</option>
@@ -265,14 +266,14 @@ const Home = () => {
         </div>
         <div className="hidden sm:block">
           <div className="border-b border-gray-200">
-            <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+            <nav className="flex space-x-8 mx-3" aria-label="Tabs">
               {tabs.map((tab) => (
                 <button
                   key={tab.name}
-                  onClick={() => setSelectedTab(tab)}
+                  onClick={() => setSelectedTab(tab.name)}
                   type="button"
                   className={classNames(
-                    tab === selectedTab
+                    tab.name === selectedTab
                       ? 'border-indigo-500 text-indigo-600'
                       : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
                     'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm',
