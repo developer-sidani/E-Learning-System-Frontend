d3f9a1c853646a3d2d9162d8d50a28af51757e77
aliyassine26 <47665097+aliyassine26@users.noreply.github.com>
2022-04-09 02:31:14 +0300

    fixed nav bar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -139,7 +139,7 @@ const Navbar = () => {
                     alt="Workflow"
                   />
                 </button>
-                <div className="hidden lg:ml-8 lg:flex lg:space-x-4">
+                <div className=" hidden lg:ml-8 lg:flex lg:space-x-4">
                   <Popover.Group as="nav" className="hidden md:flex space-x-10">
                     <Popover className="relative">
                       {({ open }) => (
@@ -169,7 +169,7 @@ const Navbar = () => {
                             leaveFrom="opacity-100 translate-y-0"
                             leaveTo="opacity-0 translate-y-1"
                           >
-                            <Popover.Panel className="absolute z-10 pl-2 mt-3 transform w-max max-w-md lg:max-w-2xl lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+                            <Popover.Panel className="absolute z-10 pl-2 mt-3 transform w-max max-w-md lg:max-w-2xl lg:ml-0 lg:left-40 lg:-translate-x-1/2">
                               <div className="ml-8 rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                                 <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
                                   {categories.map((category, index) => (
@@ -313,7 +313,7 @@ const Navbar = () => {
                         leaveFrom="opacity-100 translate-y-0"
                         leaveTo="opacity-0 translate-y-1"
                       >
-                        <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
+                        <Popover.Panel className="absolute z-10 left-[-200%] transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
                           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                             <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
                               {cart?.courses?.map((cart, index) => (
