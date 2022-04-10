13d5b733a2df58dc555bbd3151f6993513f10856
Hasan Itani <74467477+HasanItani@users.noreply.github.com>
2022-04-10 15:36:52 +0300

    changes on cart page

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,15 +1,15 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/solid'
 
-const products = [
+const courses = [
   {
     id: 1,
     name: 'Artwork Tee',
     href: '#',
     price: '$32.00',
-    color: 'Mint',
-    size: 'Medium',
+    instructor: 'Punjab Singh',
+    rating: 'stars',
     inStock: true,
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/checkout-page-03-product-04.jpg',
+    imageSrc: 'https://img-c.udemycdn.com/course/240x135/1331946_ed41_4.jpg',
     imageAlt: 'Front side of mint cotton t-shirt with wavey lines pattern.',
   },
   {
@@ -17,35 +17,35 @@ const products = [
     name: 'Basic Tee',
     href: '#',
     price: '$32.00',
-    color: 'Charcoal',
+    instructor: 'Fidel Castro',
     inStock: false,
     leadTime: '7-8 years',
-    size: 'Large',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-02.jpg',
+    rating: 'stars',
+    imageSrc: 'https://img-c.udemycdn.com/course/240x135/1331946_ed41_4.jpg',
     imageAlt: 'Front side of charcoal cotton t-shirt.',
   },
-  // More products...
+  // More courses...
 ]
 
 const CartComponent = () => (
  
     <div className="bg-white">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-0">
-        <h1 className="text-3xl font-extrabold text-center tracking-tight text-gray-900 sm:text-4xl">Shopping Cart</h1>
+        <h1 className="text-3xl font-extrabold text-center tracking-tight text-gray-900 sm:text-4xl">Course Cart</h1>
 
         <form className="mt-12">
           <section aria-labelledby="cart-heading">
             <h2 id="cart-heading" className="sr-only">
-              Items in your shopping cart
+              Courses in your shopping cart
             </h2>
 
             <ul role="list" className="border-t border-b border-gray-200 divide-y divide-gray-200">
-              {products.map((product) => (
-                <li key={product.id} className="flex py-6">
+              {courses.map((course) => (
+                <li key={course.id} className="flex py-6">
                   <div className="flex-shrink-0">
                     <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
+                      src={course.imageSrc}
+                      alt={course.imageAlt}
                       className="w-24 h-24 rounded-md object-center object-cover sm:w-32 sm:h-32"
                     />
                   </div>
@@ -54,25 +54,25 @@ const CartComponent = () => (
                     <div>
                       <div className="flex justify-between">
                         <h4 className="text-sm">
-                          <a href={product.href} className="font-medium text-gray-700 hover:text-gray-800">
-                            {product.name}
+                          <a href={course.href} className="font-medium text-gray-700 hover:text-gray-800">
+                            {course.name}
                           </a>
                         </h4>
-                        <p className="ml-4 text-sm font-medium text-gray-900">{product.price}</p>
+                        <p className="ml-4 text-sm font-medium text-gray-900">{course.price}</p>
                       </div>
-                      <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                      <p className="mt-1 text-sm text-gray-500">{product.size}</p>
+                      <p className="mt-1 text-sm text-gray-500">{course.instructor}</p>
+                      <p className="mt-1 text-sm text-gray-500">{course.rating}</p>
                     </div>
 
                     <div className="mt-4 flex-1 flex items-end justify-between">
                       <p className="flex items-center text-sm text-gray-700 space-x-2">
-                        {product.inStock ? (
+                        {/* {course.inStock ? (
                           <CheckIcon className="flex-shrink-0 h-5 w-5 text-green-500" aria-hidden="true" />
                         ) : (
                           <ClockIcon className="flex-shrink-0 h-5 w-5 text-gray-300" aria-hidden="true" />
-                        )}
+                        )} */}
 
-                        <span>{product.inStock ? 'In stock' : `Will ship in ${product.leadTime}`}</span>
+                        {/* <span>{course.inStock ? 'In stock' : `Will ship in ${course.leadTime}`}</span> */}
                       </p>
                       <div className="ml-4">
                         <button type="button" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
@@ -99,7 +99,7 @@ const CartComponent = () => (
                   <dd className="ml-4 text-base font-medium text-gray-900">$96.00</dd>
                 </div>
               </dl>
-              <p className="mt-1 text-sm text-gray-500">Shipping and taxes will be calculated at checkout.</p>
+              {/* <p className="mt-1 text-sm text-gray-500">Shipping and taxes will be calculated at checkout.</p> */}
             </div>
 
             <div className="mt-10">
@@ -115,7 +115,7 @@ const CartComponent = () => (
               <p>
                 or{' '}
                 <a href="#" className="text-indigo-600 font-medium hover:text-indigo-500">
-                  Continue Shopping<span aria-hidden="true"> &rarr;</span>
+                  Continue Browsing for Courses<span aria-hidden="true"> &rarr;</span>
                 </a>
               </p>
             </div>
