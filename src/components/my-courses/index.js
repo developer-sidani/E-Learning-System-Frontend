84092e2d3e0b1d2f8f077444e997b8b8841ce202
aliyassine26 <47665097+aliyassine26@users.noreply.github.com>
2022-04-16 22:43:40 +0300

    my-courses page:
    added grid

--- src/components/my-courses/index.js
+++ src/components/my-courses/index.js
@@ -1,3 +1,4 @@
 import MyCoursesComponent from './my-courses'
 
+export { Pagination } from './pagination'
 export { MyCoursesComponent }
--- src/components/my-courses/my-courses.jsx
+++ src/components/my-courses/my-courses.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Tab } from '@headlessui/react'
+import { StarIcon } from '@heroicons/react/solid'
+import { Pagination } from '.'
+import { instructor, course } from './data'
+// import { Pagination } from '.'
 
-const MyCoursesComponent = () => (
-    <div>
-      MyCoursesComponent
+const MyCoursesComponent = () => {
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  return (
+    // <Tab.Panels as={Fragment}>
+    // <Tab.Panel className="mb-10">
+    <div className="mb-10">
+      {/* <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8"> */}
+      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">Products</h2>
+
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {course.map((course) => (
+            <div
+              className="rounded-lg overflow-hidden shadow-lg group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50 max-w-xs w-[270] sm:w-[160]"
+            >
+              <img
+                src={course?.image_480x270}
+                className="w-full"
+                alt="ok"
+              />
+              <div className={classNames('p-2 w-full', course?.flag?.length === 0 && 'mb-4')}>
+                <h2 className="text-md transition-all duration-100 ease-in-out font-[500] group-hover:font-bold text-clip max-w-xs">
+                  {course?.title}
+                </h2>
+                <p className="max-w-md font-extralight text-sm">
+                  {course?.instructor}
+                </p>
+                <div className="flex items-center text-clip max-w-xs gap-3">
+                  <p className="font-[500] text-sm">
+                    {course?.rating?.toString()}
+                  </p>
+                  <div className="flex items-center">
+                    {[0, 1, 2, 3, 4].map((rating) => (
+                      <StarIcon
+                        key={rating}
+                        className={classNames(
+                          parseFloat(course?.rating) > rating ? 'text-yellow-400' : 'text-gray-200',
+                          'flex-shrink-0 h-5 w-5',
+                        )}
+                        aria-hidden="true"
+                      />
+                    ))}
+                  </div>
+                </div>
+                <p className="font-bold text-sm p-0">
+                  {`${course?.currency} ${course?.price}`}
+                </p>
+                {course?.flag?.length > 0 ? (
+                  <div className="w-1/4 bg-yellow-300 px-3 py-1 mt-1 object-contain">
+                    <p
+                      className="text-xs font-[500]"
+                    >
+                      {course?.flag}
+                    </p>
+                  </div>
+                ) : (
+                  <div className="px-3 py-1 mt-1"><p /></div>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <Pagination />
+      </div>
     </div>
-)
+  )
+}
 
 export default MyCoursesComponent
