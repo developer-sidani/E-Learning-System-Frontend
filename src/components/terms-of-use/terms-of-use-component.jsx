2d29f4ac079bde0d0778240dd4e51ddadc4f1675
aliyassine26 <47665097+aliyassine26@users.noreply.github.com>
2022-04-25 12:42:49 +0300

    terms-of-use-component.jsx

--- src/components/terms-of-use/terms-of-use-component.jsx
+++ src/components/terms-of-use/terms-of-use-component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TermsOfUseComponent = ({ data }) => (
   <div className="px-20 bg-gray-100 py-5 pb-20">
-    <p className="font-extrabold text-3xl md:text-5xl my-2">
+    <p className="font-extrabold text-3xl md:text-5xl mb-8">
            <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#0A003C] to-[#9C4DF4]">
                 Terms of use
            </span>
@@ -12,10 +12,10 @@ const TermsOfUseComponent = ({ data }) => (
       {data?.overview}
       {data?.sections?.map((section, index) => (
         <div key={index}>
-          <li className="text-2xl font-bold text-[#0A003C] ">{section.title}</li>
+          <li className="text-2xl font-bold text-[#0A003C] my-8">{section.title}</li>
           <p>{section.description}</p>
           {section?.subsection && (
-            <ul className="list-disc">
+            <ul className="list-disc my-8">
               {section?.subsection?.map((x, i) => (
                 <div key={i}>
                   <li className="text-1xl font-extrabold text-[#0A003C] list-inside">{x?.title}</li>
